45ca5173b705bc1bc27cac544eee2d2262cbe74e
Yutosaki <sasayu8190@gmail.com>
2025-06-29 21:11:00 +0900

    feat: add second

---
+++ atcoder/abc/b/06-28-2.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextString() string {
+  sc.Scan()
+  return sc.Text()
+}
+
+func main(){
+  sc.Split(bufio.ScanWords)
+  
+  s := nextString()
+  t := nextString()
+
+  srunes := []rune(s)
+
+  has := true
+
+  for i := 2; i < len(srunes); i++{
+    if srunes[i] >= 65 && srunes[i] <= 90{
+      has = strings.ContainsRune(t, srunes[i-1])
+      if !has {
+        break 
+      }
+    }
+  }
+
+  if has {
+    fmt.Println("Yes")
+  }else {
+    fmt.Println("No")
+  }
+}
