2bc273277dbbaaf92c0769e6a5108249b45acb1b
Yutosaki <sasayu8190@gmail.com>
2025-07-04 15:28:04 +0900

    feat: add second

---
+++ atcoder/abc/a/07-04-2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+  s:=""
+  fmt.Scan(&s)
+
+  seen := make([]bool, 26)
+
+  for _, char := range s{
+    seen[char - 'a'] = true
+  }
+
+  for i:= 0;i<26;i++{
+    if !seen[i] {
+      fmt.Println(string('a' + i))
+      return
+    }
+  }
+}
