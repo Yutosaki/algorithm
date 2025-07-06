a5fe034de1284e5a198459809d392bec80310213
Yutosaki <sasayu8190@gmail.com>
2025-07-06 18:25:56 +0900

    chore: formatter

--- atcoder/abc/a/07-04-2.go
+++ atcoder/abc/a/07-04-2.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func main(){
-  s:=""
-  fmt.Scan(&s)
+func main() {
+	s := ""
+	fmt.Scan(&s)
 
-  seen := make([]bool, 26)
+	seen := make([]bool, 26)
 
-  for _, char := range s{
-    seen[char - 'a'] = true
-  }
+	for _, char := range s {
+		seen[char-'a'] = true
+	}
 
-  for i:= 0;i<26;i++{
-    if !seen[i] {
-      fmt.Println(string('a' + i))
-      return
-    }
-  }
+	for i := 0; i < 26; i++ {
+		if !seen[i] {
+			fmt.Println(string('a' + i))
+			return
+		}
+	}
 }
