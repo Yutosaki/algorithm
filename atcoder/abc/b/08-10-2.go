63c04c0f105741f2cc8fda3693500d379a7d0f64
Yutosaki <sasayu8190@gmail.com>
2025-08-10 21:39:15 +0900

    feat: second

---
+++ atcoder/abc/b/08-10-2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  S:=""
+  fmt.Scan(&S)
+
+  var res float64
+  for i:=0;i<len(S)-2;i++{
+    for j:=i+2;j<len(S);j++{
+      if S[i] == 't' && S[j] == 't'{
+        t_len := j - i + 1
+        x := 0
+        for k:=i;k<=j;k++{
+          if S[k] == 't'{
+            x++
+          }
+        }
+        tmp := (float64(x-2)/ float64(t_len - 2))
+
+        if res < tmp{
+          res = tmp
+        }
+      }
+    }
+  }
+
+  fmt.Println(res)
+}
