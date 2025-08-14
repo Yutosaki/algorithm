0424f652fe697447ecf2d62257ba94d802fc6679
Yutosaki <sasayu8190@gmail.com>
2025-08-14 09:50:44 +0900

    fix: formatter

--- atcoder/abc/b/08-13-2.go
+++ atcoder/abc/b/08-13-2.go
@@ -2,21 +2,21 @@ package main
 
 import "fmt"
 
-func main(){
-  n:=0
-  fmt.Scan(&n)
+func main() {
+	n := 0
+	fmt.Scan(&n)
 
-  s:=""
-  for i:=0;i<n;i++{
-    c,l:="",0
-    fmt.Scan(&c,&l)
-    if len(s) + l > 100{
-      fmt.Println("Too Long")
-      return
-    }
-    for j:=0;j<l;j++{
-      s+=c
-    }
-  }
-  fmt.Println(s)
+	s := ""
+	for i := 0; i < n; i++ {
+		c, l := "", 0
+		fmt.Scan(&c, &l)
+		if len(s)+l > 100 {
+			fmt.Println("Too Long")
+			return
+		}
+		for j := 0; j < l; j++ {
+			s += c
+		}
+	}
+	fmt.Println(s)
 }
